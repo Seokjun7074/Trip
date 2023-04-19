9b9fcec939ab0364a9a4316a4aad230ab2ed550b
이석준 <sukjune96@gmail.com>
2023-04-20 00:07:09 +0900

    feat: useInput 커스텀 훅

--- src/components/organisms/PostInputBox/index.tsx
+++ src/components/organisms/PostInputBox/index.tsx
@@ -1,17 +1,26 @@
+import useInput from "hooks/useInput";
 import { ContentInput, Divider, InputLabel, PostInputWrapper, SubmitButton, TitleInput } from "./style";
 
 const PostInputBox = () => {
+  const [title, titleHandler] = useInput("");
+  const [content, contentHandler] = useInput("");
+
+  const submit = () => {
+    console.log(title);
+    console.log(content);
+  };
+
   return (
     <PostInputWrapper>
       <Divider>
         <InputLabel>제목</InputLabel>
-        <TitleInput type="text" placeholder="제목" onChange={() => {}} />
+        <TitleInput type="text" placeholder="제목" onChange={titleHandler} />
       </Divider>
       <Divider>
         <InputLabel>본문 작성</InputLabel>
-        <ContentInput type="text" placeholder="내용" onChange={() => {}} />
+        <ContentInput type="text" placeholder="내용" onChange={contentHandler} />
       </Divider>
-      <SubmitButton>작성하기</SubmitButton>
+      <SubmitButton onClick={submit}>작성하기</SubmitButton>
     </PostInputWrapper>
   );
 };
