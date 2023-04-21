337d90d6e2396b3ec05453299e240b423051e1b4
이석준 <sukjune96@gmail.com>
2023-04-21 23:04:47 +0900

    fix: 라우터 설정 수정 [#1]

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import Home from "pages/Home";
 import Post from "pages/Post";
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>{<Route path="/" element={<Home />} />}</Routes>
-      <Routes>{<Route path="/post" element={<Post />} />}</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
     </BrowserRouter>
   );
 };
