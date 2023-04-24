0a98acbe26b729f9db7377ca5ed961e3cc06d3d4
이석준 <sukjune96@gmail.com>
2023-04-24 23:28:40 +0900

    feat: 로그인UI 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import Home from "pages/Home";
+import Login from "pages/Login";
 import Post from "pages/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<Post />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { LoginWrapper } from "./style";
+import LoginInputBox from "components/organisms/LoginInputBox";
 
 const Login = () => {
-  return <>Login</>;
+  return (
+    <LoginWrapper>
+      <LoginInputBox></LoginInputBox>
+    </LoginWrapper>
+  );
 };
 export default Login;
