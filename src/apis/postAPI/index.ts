5ef2453ea4603fbf5f4d6e11871efd838aceccce
이석준 <sukjune96@gmail.com>
2023-04-25 23:04:45 +0900

    feat: 게시글 관련 API 생성

--- src/apis/postAPI/index.ts
+++ src/apis/postAPI/index.ts
@@ -8,3 +8,11 @@ export const getPost = (id: number) => API.get(`/posts/${id}`);
 export const editPost = (id: number, data: editPostInterface) => API.put(`/posts/${id}`, data);
 
 export const deletePost = (id: number) => API.delete(`/posts/${id}`);
+
+export const likePost = (id: number) => API.post(`/posts/${id}/like`);
+
+export const searchPost = (page: number, size: number, keyword: string) =>
+  API.post(`/posts/search?page=${page}&size=${size}&keyword=${keyword}`);
+
+export const getAllPost = (page: number, size: number, sort: string) =>
+  API.get(`/board?page=${page}&size=${size}&sort=${sort}`);
