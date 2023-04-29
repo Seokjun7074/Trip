4f8ff329d5572e4e475245f6ab562c4cf38c0fa5
이석준 <sukjune96@gmail.com>
2023-04-29 21:54:48 +0900

    feat: Card 컴포넌트 생성

---
+++ src/components/organisms/Card/style.ts
@@ -0,0 +1,8 @@
+import FlexBox from "components/atoms/FlexBox";
+import styled from "styled-components";
+
+export const CardWrapper = styled(FlexBox)`
+  width: 300px;
+  height: 100px;
+  border-radius: 10px;
+`;
