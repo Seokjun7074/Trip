3a7fd8d78e5986dcec7ec294dad44b2b44d4d312
Seokjun7074 <sukjune96@gmail.com>
2023-05-01 12:00:48 +0900

    feat: json-server 메인페이지 연결

--- src/components/organisms/GridBox/index.tsx
+++ src/components/organisms/GridBox/index.tsx
@@ -3,83 +3,32 @@ import Card from "../Card";
 import { Grid, GridBoxWrapper } from "./style";
 import { getBoard } from "apis/postAPI";
 
-const dummy = [
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-  {
-    title: "제목",
-  },
-];
+const initeState = {
+  postId: 1,
+  registerTime: "yyyy-MM-dd HH:mm:ss",
+  writer: "작성자1",
+  title: "제목",
+  views: 1,
+  likes: 1,
+};
 
 const GridBox = () => {
-  const [data, setData] = useState([]);
+  const [notices, setNotices] = useState([initeState]);
+  const [boards, setBoards] = useState([initeState]);
 
   useEffect(() => {
     const fetchData = getBoard(0, 0).then((res) => {
-      console.log(res.data);
+      // console.log(res.data.posts.notices);
+      setBoards(res.data.posts.boards);
+      setNotices(res.data.posts.notices);
     });
   }, []);
 
   return (
     <GridBoxWrapper>
       <Grid>
-        {dummy.map((e, idx) => (
-          <Card key={idx} title={e.title}></Card>
+        {boards.map((board) => (
+          <Card key={board.postId} title={board.title}></Card>
         ))}
       </Grid>
     </GridBoxWrapper>
