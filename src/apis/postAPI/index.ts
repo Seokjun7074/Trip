65e058555d4a195fddad162d14de7b2891d2d2bb
Seokjun7074 <sukjune96@gmail.com>
2023-05-03 16:47:34 +0900

    feat: 댓글 관련 API 생성

--- src/apis/postAPI/index.ts
+++ src/apis/postAPI/index.ts
@@ -1,5 +1,5 @@
 import API from "../axios";
-import { setPostInterface, editPostInterface, getBoardInterface } from "./postInterface";
+import { setPostInterface, editPostInterface } from "./postInterface";
 
 export const boardAPI = {
   // getBoard: (page: number, size: number) => API.post(`/board?page=${page}&size=${size}&sort=recent`),
@@ -19,4 +19,13 @@ export const boardAPI = {
     API.post(`/posts/search?page=${page}&size=${size}&keyword=${keyword}`),
 
   getAllPost: (page: number, size: number, sort: string) => API.get(`/board?page=${page}&size=${size}&sort=${sort}`),
+};
+
+export const commentAPI = {
+  setComment: (postId: number, data: { content: string }) => API.post(`/posts/${postId}/comments`, data),
+
+  editComment: (postId: number, commentId: number, data: { content: string }) =>
+    API.put(`/posts/${postId}/comments/${commentId}`, data),
+
+  deleteComment: (postId: number, commentId: number) => API.delete(`/posts/${postId}/comments/${commentId}`),
 };
