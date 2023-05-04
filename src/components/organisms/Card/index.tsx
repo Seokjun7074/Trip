d3c948557df0ce8415fd64099593256f6817d73b
Seokjun7074 <sukjune96@gmail.com>
2023-05-04 09:53:41 +0900

    style: Card 컴포넌트 CSS 수정

--- src/components/organisms/Card/index.tsx
+++ src/components/organisms/Card/index.tsx
@@ -1,5 +1,6 @@
 import Label from "components/atoms/Label";
 import { CardWrapper } from "./style";
+import Img from "components/atoms/Img";
 
 interface Props {
   children?: React.ReactNode;
@@ -8,6 +9,7 @@ interface Props {
 const Card = ({ children, title }: Props) => {
   return (
     <CardWrapper>
+      <Img src="https://dummyimage.com/600x400/000/fff" />
       <Label>{title}</Label>
     </CardWrapper>
   );
--- src/components/organisms/Card/style.ts
+++ src/components/organisms/Card/style.ts
@@ -3,8 +3,16 @@ import styled from "styled-components";
 
 export const CardWrapper = styled(FlexBox)`
   width: 300px;
-  height: 100px;
-  border-radius: 10px;
+  height: 250px;
+  flex-direction: column;
+  gap: 10px;
+  border-radius: 5px;
   border: 1px solid grey;
+  overflow: hidden;
+  color: black;
   cursor: pointer;
+  transition: box-shadow 0.25s ease-in 0s, transform 0.25s ease-in 0s;
+  :hover {
+    transform: scale(1) translate(0, -5%);
+  }
 `;
